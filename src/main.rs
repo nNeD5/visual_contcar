a1f99ad4d7451dc0c39cdafea1655385ae0ca081
ned <oned.reg@gmail.com>
2024-08-03 00:46:26 +0300

    parse cmd

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 //use raylib::prelude::*;
-use std::fs;
+use std::{process::ExitCode, fs, env};
 
 fn parse_poscar(path: &str) -> Vec<Vec<f32>> {
     fs::read_to_string(path)
         .expect("Unable to read {path}")
         .lines()
         .skip(5)
-        .skip_while(|&l| !l.starts_with(['d', 'c', 'D', 'C']))
+        .skip_while(|l| !l.starts_with(['d', 'c', 'D', 'C']))
         .skip(1)
+        .take_while(|l| l.trim().len() > 0)
         .map(|l|
             l.split_whitespace()
                 .filter_map(|i| i.trim().parse::<f32>().ok())
@@ -16,9 +17,13 @@ fn parse_poscar(path: &str) -> Vec<Vec<f32>> {
         .collect()
 }
 
-fn main() {
-    //parse_poscar("POSCAR");
-    println!("{:?}", parse_poscar("POSCAR"));
+fn main() -> ExitCode {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("{} usage: visual_poscar path_to_poscar", args[0]);
+        return ExitCode::from(1);
+    }
+    println!("Points: {:?}", parse_poscar("POSCAR_2"));
     //let w = 1200;
     //let h = 800;
     //let r = 20.0;
@@ -32,4 +37,5 @@ fn main() {
     //    d.clear_background(Color::BLACK);
     //    d.draw_circle(h/2, h/2, r, Color::RED);
     //}
+    ExitCode::SUCCESS
 }
